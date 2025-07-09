67d15cf991505ff2a5e040bb36edf950526eea3d
Vitor Costa Duarte <118634941+VitorDuarte93@users.noreply.github.com>
2025-07-09 11:09:35 -0300

    Update servidor.js
    adicionado o controller do funcionario

--- rotas/servidor.js
+++ rotas/servidor.js
@@ -1,17 +1,16 @@
-// aqui fica o server.js
-
 const express = require('express');
 const cors = require('cors');
 const ciclistaRoutes = require('../controllers/ciclistaController');
 const aluguelRoutes = require('../controllers/aluguelController');
+const funcionarioRoutes = require('../controllers/funcionarioController'); // 👈 Adicionado
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/ciclista', ciclistaRoutes);
-
 app.use('/aluguel', aluguelRoutes);
+app.use('/funcionario', funcionarioRoutes); // 👈 Adicionado
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
