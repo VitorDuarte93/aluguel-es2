cc1b682fb9c34ed1fa27eb19b298df49f887d21f
Vitor Costa Duarte <118634941+VitorDuarte93@users.noreply.github.com>
2025-07-09 14:15:09 -0300

    Update aluguel.js
    .

--- services/aluguel.js
+++ services/aluguel.js
@@ -1,43 +1,35 @@
-const database = require('../repositories/acessoDB/aluguelDB')
+const database = require('../repositories/acessoDB/aluguelDB');
+
 async function alugarBicicleta(idCiclista, idTranca) {
     let bicicleta = await database.recuperaBicicletaPorTranca(idTranca);
 
-    if (bicicleta.status === "disponível"){
-        //ciclista já possui um aluguel ativo?
+    if (bicicleta.status === "disponível") {
         let aluguelAtivo = await database.verificaAluguelAtivo(idCiclista);
-        if (!aluguelAtivo){
-            //pega a data de início, pega a tranca, cria e paga a cobrança (externos), libera a bicicleta
+        if (!aluguelAtivo) {
             let dataInicio = new Date().toISOString();
-            //aqui a gente chamaria o externos -> cobrança.
             await database.registraAluguel(idCiclista, bicicleta.id, dataInicio);
-
         } else {
-            //tem aluguel ativo
-            throw new Error ('Já possui um aluguel ativo. Finalizar o aluguel atual antes de alugar uma nova bicicleta.');
+            throw new Error('Já possui um aluguel ativo. Finalizar o aluguel atual antes de alugar uma nova bicicleta.');
         }
-
     } else {
-        // bota que não tá disponível
         throw new Error('Bicicleta não disponível para aluguel');
     }
-
 }
 
 async function devolverBicicleta(trancaFim, idBicicleta) {
-    // 1. Verifica se o ciclista tem um aluguel ativo
-    let aluguelAtivo = await database.verificaCiclistaComBicicleta(idBicicleta);
-    if (!aluguelAtivo) {
+    let idCiclista = await database.verificaCiclistaComBicicleta(idBicicleta);
+    if (!idCiclista) {
         throw new Error('Nenhum aluguel ativo encontrado para essa bicicleta');
     }
 
-    // 2. Atualiza o horário de fim do aluguel
+    const aluguel = {
+        ciclista: idCiclista
+    };
+
     let dataFim = new Date().toISOString();
-    await database.finalizarAluguel(aluguelAtivo, trancaFim, dataFim);
+    await database.finalizarAluguel(aluguel, trancaFim, dataFim);
 
-    // 3. Atualiza o status da bicicleta para "disponível"
     await database.atualizarStatusBicicleta(idBicicleta, 'disponível');
-
-    //equipamentos -> tranca -> trancar
 
     return { mensagem: 'Bicicleta devolvida com sucesso' };
 }
@@ -45,4 +37,4 @@ async function devolverBicicleta(trancaFim, idBicicleta) {
 module.exports = {
     alugarBicicleta,
     devolverBicicleta
-}
+};
